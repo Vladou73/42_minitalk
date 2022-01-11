745769d9245bd6b4341602421b3f1e5046ef0c40
Vladimir NAFISSI <vnafissi@e3r4p6.42.fr>
2022-01-11 17:41:13 +0100

    implemented only 1 malloc

--- server.c
+++ server.c
@@ -2,35 +2,58 @@
 
 static char *msg;
 
-static	int handle_msg_size(int sigtype, uint32_t count_signals, int bin_index, char is_first_signal)
+static	int handle_msg_size(int sigtype, uint32_t *count_signals, char *is_first_signal)
 {
 	static	uint32_t len_msg;
-	
-	ft_putstr_fd("check", 1);
-	if (is_first_signal == '\0')
+
+	if (*is_first_signal == '\0')
+		*is_first_signal = '1';
+
+	len_msg |= (uint32_t)((sigtype == SIGUSR1) << (31 - *count_signals));
+	(*count_signals)++;
+	if (*count_signals == 32)
 	{
-		bin_index = 31;
-		is_first_signal = '1';
-	}
-	len_msg |= (uint32_t)((sigtype == SIGUSR1) << bin_index);
-	bin_index--;
-	count_signals++;
-	if (bin_index == 0)
-	{
-		is_first_signal = '\0';
-		//allocate full size of message
-		msg = malloc(len_msg + 4); // + 4 pour le caractere NULL a la fin ?
+		*is_first_signal = '\0';
+		msg = malloc(sizeof(char) * (len_msg + 1)); //allocate full size of message
 		if (!msg)
 			return (1);
-		ft_memset(msg, 0, len_msg + 4);
+		ft_memset(msg, 0, sizeof(char) * (len_msg + 1));
 	}
 	return (0);
 }
 
+static	void store_print_msg(int sigtype, uint32_t *count_signals, char *is_first_signal)
+{
+	static	int bin_index;
+	static	char c;
+
+	if (*is_first_signal == '\0')
+	{
+		bin_index = 7;
+		*is_first_signal = '1';
+	}
+	if (bin_index >= 0)
+	{
+		c |= (char)((sigtype == SIGUSR1) << bin_index);
+		bin_index--;
+	}
+	if (bin_index < 0)
+	{
+		msg[ft_strlen(msg)] = c;
+		*is_first_signal = '\0';
+		if (c == '\0')
+		{
+			ft_putstr_fd(msg, 1);
+			ft_free_null_ptr(&msg);
+			*count_signals = 0;
+			bin_index = 0;
+		}
+		c = 0;
+	}
+}
+
 static void handler_sigusr_bis(int sigtype, siginfo_t *siginfo, void *ucontext)
 {
-	static	char c;
-	static	int bin_index;
 	static	char is_first_signal;
 	static	uint32_t count_signals;
 
@@ -41,30 +64,9 @@ static void handler_sigusr_bis(int sigtype, siginfo_t *siginfo, void *ucontext)
 	//kill(siginfo->si_pid, SIGUSR1); //systeme ping pong -> pas sur de l'utiliser
 	
 	if (count_signals < 32)
-		handle_msg_size(sigtype, count_signals, bin_index, is_first_signal);
-
-	if (is_first_signal == '\0')
-	{
-		bin_index = 7;
-		is_first_signal = '1';
-	}
-	if (bin_index >= 0)
-	{
-		c |= (char)((sigtype == SIGUSR1) << bin_index);
-		bin_index--;
-	}
-	if (bin_index < 0)
-	{
-		//msg = ft_strjoin_free(&msg, c);
-		msg[ft_strlen(msg)] = c;
-		is_first_signal = '\0';
-	if (c == '\0')
-	{
-		ft_putstr_fd(msg, 1);
-		ft_free_null_ptr(&msg);
-	}
-	c = 0;
-	}
+		handle_msg_size(sigtype, &count_signals, &is_first_signal);
+	else
+		store_print_msg(sigtype, &count_signals, &is_first_signal);
 }
 
 int main()
